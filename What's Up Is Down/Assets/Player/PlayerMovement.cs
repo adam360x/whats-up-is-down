1bce322214576ba0f781088205c0665ceae4a0d2
kittytang94 <lptang@wisc.edu>
2020-10-24 23:18:17 -0500

    Level scene and player buttons, attempted player movements

---
+++ What's Up Is Down/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+
+    bool jump = false;
+    float horizontalMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("JumpButton"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Moving character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
