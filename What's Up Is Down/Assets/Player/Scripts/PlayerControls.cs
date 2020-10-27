37bc44020823d1a09cbe080e93cc03217a72b285
kittytang94 <lptang@wisc.edu>
2020-10-26 23:28:14 -0500

    Pause screen and organized some stuff

---
+++ What's Up Is Down/Assets/Player/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gravity to left
+        if ()
+        {
+            Physics2D.gravity = new Vector2(Physics2D.gravity.y, 0);
+        }
+        //gravity to right
+        if () 
+        { 
+            Physics2D.gravity = new Vector2(Physics2D.gravity.x, 0);
+        }
+    }
+}
