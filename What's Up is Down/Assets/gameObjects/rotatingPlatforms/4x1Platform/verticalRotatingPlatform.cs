7401d09a21945346172eaba370adf638d4258012
SimonT321 <43785940+SimonT321@users.noreply.github.com>
2020-11-18 11:05:36 -0600

    Tried fixing pendulum platform. Made a vertical starting position for the rotating platform

---
+++ What's Up is Down/Assets/gameObjects/rotatingPlatforms/4x1Platform/verticalRotatingPlatform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class verticalRotatingPlatform : MonoBehaviour
+{
+    Collider2D col;
+    public float rotationSpeed;
+    public float rotZ = 90;
+    public bool rotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Collider2D touchedCollider = Physics2D.OverlapPoint(wp);
+            if (col == touchedCollider)
+            {
+                rotate = true;
+                Debug.Log("Hello");
+            }
+        }
+        if (rotate)
+        {
+            rotZ += -5f;
+            transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+            if (rotZ % 90 == 0)
+            {
+                rotate = false;
+            }
+        }
+    }
+}
