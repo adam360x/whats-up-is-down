2a4d7dd82846c7d4c9be20e72181fcbc18b12cc2
kittytang94 <lptang@wisc.edu>
2020-11-27 13:01:46 -0600

    Pause Menu created

---
+++ What's Up Is Down/Assets/PauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool IsPaused = false;
+
+    // Update is called once per frame
+    public void pauseGame()
+    {
+        if (IsPaused)
+        {
+            Time.timeScale = 1;
+            IsPaused = false;
+        } else
+        {
+            Time.timeScale = 0;
+            IsPaused = true;
+        }
+    }
+}
