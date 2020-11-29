7ff7eb18116de5699c424a0507fafd5fe4d80379
Alzomaili <48036688+Alzomaili@users.noreply.github.com>
2020-11-29 17:25:59 -0600

    fixed pause menu on lvls 1-3
    Everything in the pause menu now works except volume control. Also, quit button in options menu is deleted since it's useless.

---
+++ What's Up Is Down/Assets/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool IsPaused = false;
+
+    // Update is called once per frame
+    public void pauseGame()
+    {
+        if (IsPaused)
+        {
+            Time.timeScale = 1;
+            IsPaused = false;
+
+        } else
+        {
+            Time.timeScale = 0;
+            IsPaused = true;
+        }
+    }
+
+    public void resetLevel()
+    {
+        if (IsPaused)
+        {
+            Time.timeScale = 1;
+            IsPaused = false;
+
+        } else
+        {
+            Time.timeScale = 0;
+            IsPaused = true;
+        }
+    }
+}
