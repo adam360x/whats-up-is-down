9131db594a60ecf58a41d38f98d6d9d99ac14da0
adam <hey>
2020-12-02 03:27:08 -0600

    Add music, volume slider functionality, music not resetting on death, and music resetting and stopping when going to main menu

---
+++ What's Up Is Down/Assets/musicScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class musicScript : MonoBehaviour
+{
+    static bool AudioBegin = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        if (!AudioBegin)
+        {
+            GetComponent<AudioSource>().Play();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        
+        if(scene.buildIndex == 0)
+        {
+            Debug.Log(scene.name);
+            GetComponent<AudioSource>().Stop();
+            AudioBegin = false;
+
+        }
+
+    }
+}
