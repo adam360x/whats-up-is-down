899c8e199bc7952014de92cd3399eed55fa67907
Alzomaili <48036688+Alzomaili@users.noreply.github.com>
2020-12-08 20:03:14 -0600

    Falling platform added to toturial level

---
+++ What's Up Is Down/Assets/gameObjects/FallingPlatforms/FallingPlatform.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    Rigidbody2D rb;
+    BoxCollider2D box;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public float dropTime;
+    public float noCollideTime;
+    public float destroyTime;
+    public float respawnTime;
+    private bool isGrounded;
+    private Vector2 startPos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        box = GetComponent<BoxCollider2D>();
+        startPos = gameObject.transform.position;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.10f, whatIsGround);
+        if(col.gameObject.tag.Equals("Player") && isGrounded){
+            Invoke("DropPlatform", dropTime);
+            Invoke("NoCollidePlat", noCollideTime);
+            Invoke("DestroyPlat", destroyTime);
+            Invoke("RespawnPlat", respawnTime);
+        }
+    }
+
+    void NoCollidePlat() {
+        box.enabled = false;
+    }
+
+    void DropPlatform(){
+        rb.isKinematic = false;
+        rb.mass = 100;
+        rb.gravityScale = 5;
+    }
+
+    void DestroyPlat(){
+        gameObject.SetActive(false);
+    }
+
+    void RespawnPlat(){
+        rb.isKinematic = true;
+        box.enabled = true;
+        gameObject.transform.position = startPos;
+        gameObject.SetActive(true);
+    }
+  
+}
---
+++ What's Up Is Down/Assets/gameObjects/TutorialObjects/post9.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class post9 : MonoBehaviour
+{
+    public static post9 instance;
+    public TextMeshProUGUI post9Text;
+    bool showText = false;
+    float count = 0;
+    public float messageTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            Debug.Log("post9Test: Instance assigned: " + instance);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (showText)
+        {
+            count += Time.deltaTime;
+            if (count >= messageTime)
+            {
+                post9Text.text = "";
+                count = 0;
+                showText = false;
+                Debug.Log("post9Test: showText is now: " + showText);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            showText = true;
+            post9Text.text = "be careful! The wood boxes can hold your weight for a short time before they give!";
+            Debug.Log("post9Test: showText is now: " + showText);
+
+        }
+
+    }
+}
